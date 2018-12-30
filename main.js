4412c7273db1b724f2afda6b6967b68902f2dcce
chi <nguyenlinhchi09@gmail.com>
2018-12-30 18:33:09 +0100

    cycle

--- main.js
+++ main.js
@@ -84,7 +84,7 @@ function step8() {
 function colorne() {
 	     nes = selectall("table td.ne");
 	     nes.forEach(function (element) { element.style = "color:#f4656d"  });
-         narrate("Can you think about the efficiency of these Nash equilibria? Like, take the point (2,8). If you choose to claim 2 I should claim 8 because it is the optimised number. If I claim x < 8, I would get x but x < 8 so I feel it is wasted. If I claim x > 8, the sum x + 2 > 10 hence we both get 0, since 0 < 8, I guess 8 is the optimal choice.", ".ans0")
+         narrate("Take the point (2,8). If you choose to claim 2 I should claim 8 because it is the optimised number. If I claim x < 8, I would get x but x < 8 so I feel it is wasted. If I claim x > 8, the sum x + 2 > 10 hence we both get 0, since 0 < 8, I guess 8 is the optimal choice if the other claims 2. Can you do the same reasoning for other contingencies of the other player?", ".ans0")
 	 }
 function answer() {
     narrate("In each round, there are 3 possible actions. Hence, for a repeated game of n rounds, the number of strategies is 3<sup>n</sup>.", "span.ans")
@@ -202,8 +202,9 @@ displaysvg(".ex2");
 
 
 function III3step0() {
-    offall("*.reset");
-    narrate("", ".cap3");
+    narrate("In the beginining, there is the population of agents.", ".cap3");
+    offone(".phase");
+    setto
 }
 
 function III3step1() {
@@ -237,4 +238,61 @@ function III3step6() {
 function III3step7() {
     onone("text.name");
     narrate("Of course it is the flexible machine.", ".cap3")
+}
+
+function answer2b() {
+    displaysvg(".ans2b")
+}
+
+function answer2c() {
+    displaysvg(".ans2c")
+}
+
+function IVstep0() {
+    narrate("In the begining there is the population of agents", ".cap0");
+    narrate("MATCHING PHASE", ".phase");
+    settored(selectone(".random"), 'fill');
+}
+
+function IVstep1() {
+    onone(".path");
+    onone(".phase");
+    narrate("We pair-match the agents randomly.", ".cap0");
+}
+
+function IVstep2() {
+    onone(".pay");
+    narrate("They use their assigned strategies to play the game we want, and get their payoffs from it", ".cap0");
+}
+function IVstep3() {
+    onone(".fitness");
+    narrate("After getting the payoffs, we use the payoffs to calculate fitness. First of all, we take the sum of all the payoffs.", ".cap0");
+}
+function IVstep4() {
+    onone(".relative");
+    narrate("We calculate the relative share of the sum for each strategy. For example, there are 5 agents using red strategy. 4 of them get 50 each and 1 of them get 0. Together 5 of them get 200 out of total 288. The relative share of the red strategy is 200/288 = 70%.", ".cap0");
+}
+
+function IVstep5() {
+    narrate("LEARNING PHASE", ".phase");
+    offone(".path");
+    offone(".pay");
+    offone(".fitness");
+    offone(".fittext");
+    onone(".learn");
+    narrate("We would use the fitness vector in the learning phase. In this phase, we randomly choose a small portion of the population and let them <i>learn</i>.", ".cap0");
+}
+
+function IVstep6() {
+    narrate("What do I mean by <i>learn</i>? These chosen agents would be able to abandon their current strategy, observe the whole population, and choose the one they feel like is doing good. Technically, they randomise over the fitness vector. If you look at the fitness vector, you would see that the red strategy has the biggest interval, if you are to throw a ball into that unit interval, the ball is very likely (70% to be exact) to fall into the red interval. Statistically, it is very likely for these two chosen agents to switch to use the red strategy after <i>learning</i>. In this case, we can see that there is one more agent using the red strategy and one less agent using the blue strategy. This is what it means to say that the better strategy would grow at the expense of the poor doers. To the limit, this statement is mathematically a differential equation.", ".cap0");
+    settored(selectone(".switch"), 'fill');
+}
+
+function IVstep7() {
+    offone(".learn");
+    offone(".relative");
+    offone(".fittext");
+    narrate("MUTATION PHASE", ".phase");
+    narrate("In the mutation phase, a small portion of the population would be randomly chosen. They would be able to mutate/experiment/make mistakes with their strategy. Which means that they would choose a completely new strategy from the strategy space to use in the pool. In the learning phase, the agent would choose from current pool, in this phase, the agent choose a completely new variant. The learning phase is to select the fittest of the current pool, the mutation phase is to keep adding new variety into the pool. Only by keeping adding new variety, can we hope to cover enough of the infinite strategy space. only by trying to cover a good part of the infinite strategy space, can we be confident enough with the optima we find using this searching method. Do you think so?", ".cap0");
+    selectone(".random").setAttributeNS(null, 'fill', 'black');
 }
